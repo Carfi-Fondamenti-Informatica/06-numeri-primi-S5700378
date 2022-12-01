d747f0bda9e32d5cef2a502e80c63b96b1b16dd4
S5700378 <117925531+S5700378@users.noreply.github.com>
2022-12-01 22:13:41 +0100

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,6 @@
 using namespace std;
 
 int main(){
-  int n=0, e=2;
-  bool f;
-  cin >> n;
-  f=primo (n,e);
-  if (f)
-  {
-    cout << "numero primo";
-  }
-  else
-  {
-    cout << "numero non primo";
-  }
+  
   return 0;
 }
