c5c2115d35c2ae07eda810eae211d1afb16f8846
S5700378 <117925531+S5700378@users.noreply.github.com>
2022-12-02 21:22:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
+#include<iostream>
 #include "lib.h"
 using namespace std;
 
 int main() {
     int p;
     cin >> p;
-if (numeroprimo(p) == 0) {
-    cout << "numero primo" << endl;
-}
-  else {
-    cout << "numero non primo" << endl;
-} 
-  return 0;
+    if (numeroprimo(p) == 0) {
+         cout << "numero primo" << endl;
+    }
+    else {
+        cout << "numero non primo" << endl;
+    } 
+    return 0;
 }
